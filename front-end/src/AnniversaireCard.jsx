581d8bdf643e4e22308843c089fe0f3015169aeb
David MARTINS <david.joaquim.martins@gmail.com>
2026-03-18 16:43:38 +0100

    FRONT Remake 2

--- front-end/src/AnniversaireCard.jsx
+++ front-end/src/AnniversaireCard.jsx
@@ -3,12 +3,28 @@ import './styles/Home.css';
 import { Link } from 'react-router-dom';
 
 const AnniversaireCard = ({id, nom, date,frequence }) => {
+
+  const calculerAge = () => {
+    if (frequence !== "Anniv'" && frequence !== "Annuel") return null;
+    const today = new Date();
+    const dateParsed = date.split('/'); // format dd/mm/yyyy
+    const annee = parseInt(dateParsed[2]);
+    const age = today.getFullYear() - annee;
+    return age;
+  };
+
+  const age = calculerAge();
+
   return (
     <div className="postit-card">
+      
       <h2>{nom}</h2>
+      {age !== null  && <span style={{ marginLeft: '0.4rem', color: '#888',marginBottom: '0.3rem' }}>({age} ans)</span>}
       <div className="editEvent">
         <div className="dateFrequence">
-          <p>{date}</p>
+          <p style={{ marginTop: '0.5rem' }}>{date}
+          </p>
+          
           <p className={
             frequence === "Anniv'" ? 'frequenceAnniv' :
             frequence === "Annuel" ? 'frequenceAnnuel' :
